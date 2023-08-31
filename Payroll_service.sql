25565a2b9a5df2795d1f13aec1659d8f10c11db0
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-31 18:24:50 +0530

    [Riya] Add : Payroll Service

--- Payroll_service.sql
+++ Payroll_service.sql
@@ -156,7 +156,29 @@ department = @department, basic_pay = @basic_pay, deductions = @deductions, taxa
 where Id=@Id
 End
 
+Create Procedure Range(
+@start_date date
+)
+As
+Begin
+Select * from employee_payroll where start_date between CAST(@start_date as Date) and GETDATE();
+End
+
+Create Procedure Calculations
+As
+Begin
+Select 
+Sum(cast(salary as bigint))as Sum , 
+Avg(cast(salary as bigint))as Avg,
+Min(cast(salary as bigint))as Min,
+Max(cast(salary as bigint))as Max from employee_payroll; 
+End
+
 EXEC GetAllEmployee_payroll;
 
 Select e.*,d.* from employee e,department d where e.Id = d.deptId and e.Id = 1;
 
+UPDATE employee_payroll
+SET gender = 'F', phone = 1092837465, address = 'Annikkanadu', department ='IT', basic_pay = 8000, 
+deductions = 2000, taxable_pay = 500, income_tax = 200, net_pay = 3000
+WHERE name = 'Riya';
